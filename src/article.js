6d5c62525505d7729a035fdceb7782c30238d00f
Stefan Wintergerst <stefan.wintergerst@team-parallax.com>
2023-10-14 11:26:35 +0200

    fix article location, lang dropdown

--- src/article.js
+++ src/article.js
@@ -21,20 +21,29 @@ export const Article = ({article}) => {
                     </svg>
                     <span>{article.starRating}</span>
                 </dd>
-                <dt class="sr-only">Location</dt>
-                <dd class="flex items-center">
-                    <svg width="2" height="2" aria-hidden="true" fill="currentColor" class="mx-3 text-slate-300">
-                        <circle cx="1" cy="1" r="1"/>
-                    </svg>
-                    <svg width="24" height="24" fill="none" stroke="currentColor" stroke-width="2"
-                         stroke-linecap="round" stroke-linejoin="round" class="mr-1 text-slate-400 dark:text-slate-500"
-                         aria-hidden="true">
-                        <path
-                            d="M18 11.034C18 14.897 12 19 12 19s-6-4.103-6-7.966C6 7.655 8.819 5 12 5s6 2.655 6 6.034Z"/>
-                        <path d="M14 11a2 2 0 1 1-4 0 2 2 0 0 1 4 0Z"/>
-                    </svg>
-                    {article.location}
-                </dd>
+                {
+                    article.location && (
+                        <>
+                            <dt className="sr-only">Location</dt>
+                            <dd className="flex items-center">
+                                <svg width="2" height="2" aria-hidden="true" fill="currentColor"
+                                     className="mx-3 text-slate-300">
+                                    <circle cx="1" cy="1" r="1"/>
+                                </svg>
+                                <svg width="24" height="24" fill="none" stroke="currentColor" stroke-width="2"
+                                     stroke-linecap="round" stroke-linejoin="round"
+                                     className="mr-1 text-slate-400 dark:text-slate-500"
+                                     aria-hidden="true">
+                                    <path
+                                        d="M18 11.034C18 14.897 12 19 12 19s-6-4.103-6-7.966C6 7.655 8.819 5 12 5s6 2.655 6 6.034Z"/>
+                                    <path d="M14 11a2 2 0 1 1-4 0 2 2 0 0 1 4 0Z"/>
+                                </svg>
+                                {article.location}
+                            </dd>
+                        </>
+                    )
+                }
+
             </dl>
             <div className="px-3">
                 <div
