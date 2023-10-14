3762093be1383ef499d6fb9ac5afc6bfe241c218
Stefan Wintergerst <stefan.wintergerst@team-parallax.com>
2023-10-14 10:30:35 +0200

    improve article page

--- src/articles.js
+++ src/articles.js
@@ -1,303 +1,330 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import {Navbar} from "./navbar"
 import {Search} from './search';
 import Shelf from "./shelf"
 import List from "./list"
 import ListItem from "./listitem"
-import {StateProvider, useStateValue } from "./state"
+import {StateProvider, useStateValue} from "./state"
+
 const moviesUa = [
-  {
-    id: 1,
-    title: "Інформація про медичне забезпечення від Федерального міністерства охорони здоров'я",
-    rating: [
-      {
-        emoji: "💊",
-        text: "Медицина"
-      },
-    ],
-    isPinned: true,
-    starRating: 13,
-    date: "23.07.2023"
-  },
-  {
-    id: 2,
-    title: "Безкоштовні SIM-карти від Telekom та Vodafone",
-    isHot: true,
-    rating: [  
-      {
-        emoji: "🤝",
-        text: "Мережі"
-      },
-      {
-        emoji: "💸",
-        text: "Безкоштовно"
-      },
-    ],
-    starRating: 53,
-    date: "13.10.2023"
-  },
-  {
-    id: 3,
-    title: "Мовні курси на місці",
-    rating: [  
-      {
-        emoji: "🇩🇪",
-        text: "Мовний курс"
-      },
-      {
-        emoji: "💸",
-        text: "Безкоштовно"
-      },
-    ],
-    starRating: 31,
-    date: "13.09.2023"
-  },
-  {
-    id: 4,
-    title: "Робота для українських біженців",
-    rating: [  
-      {
-        emoji: "👩‍💼",
-        text: "Робота"
-      },
-    ],
-    starRating: 16,
-    date: "2.08.2023"
-  },
-  {
-    id: 5,
-    title: "Безкоштовна видача їжі",
-    rating: [  
-      {
-        emoji: "🍕",
-        text: "Продукти харчування"
-      },
-      {
-        emoji: "💸",
-        text: "Безкоштовно"
-      },
-    ],
-    starRating: 15,
-    date: "2.08.2023"
-  },
-  {
-    id: 6,
-    title: "Центральний пункт прибуття у Виставковому павільйоні 6",
-    rating: [  
-      {
-        emoji: "🏠",
-        text: "Проживання"
-      }
-    ],
-    starRating: 7,
-    date: "2.05.2023"
-  },
+    {
+        id: 1,
+        title: "Інформація про медичне забезпечення від Федерального міністерства охорони здоров'я",
+        rating: [
+            {
+                emoji: "💊",
+                text: "Медицина"
+            },
+        ],
+        isPinned: true,
+        starRating: 13,
+        date: "23.07.2023",
+        location: "Ulm, Baden-Württemberg"
+    },
+    {
+        id: 2,
+        title: "Безкоштовні SIM-карти від Telekom та Vodafone",
+        isHot: true,
+        rating: [
+            {
+                emoji: "🤝",
+                text: "Мережі"
+            },
+            {
+                emoji: "💸",
+                text: "Безкоштовно"
+            },
+        ],
+        starRating: 53,
+        date: "13.10.2023",
+        location: "Ulm, Baden-Württemberg"
+    },
+    {
+        id: 3,
+        title: "Мовні курси на місці",
+        rating: [
+            {
+                emoji: "🇩🇪",
+                text: "Мовний курс"
+            },
+            {
+                emoji: "💸",
+                text: "Безкоштовно"
+            },
+        ],
+        starRating: 31,
+        date: "13.09.2023",
+        location: "Ulm, Baden-Württemberg"
+    },
+    {
+        id: 4,
+        title: "Робота для українських біженців",
+        rating: [
+            {
+                emoji: "👩‍💼",
+                text: "Робота"
+            },
+        ],
+        starRating: 16,
+        date: "2.08.2023",
+        location: "Ulm, Baden-Württemberg"
+    },
+    {
+        id: 5,
+        title: "Безкоштовна видача їжі",
+        rating: [
+            {
+                emoji: "🍕",
+                text: "Продукти харчування"
+            },
+            {
+                emoji: "💸",
+                text: "Безкоштовно"
+            },
+        ],
+        starRating: 15,
+        date: "2.08.2023",
+        location: "Ulm, Baden-Württemberg"
+    },
+    {
+        id: 6,
+        title: "Центральний пункт прибуття у Виставковому павільйоні 6",
+        rating: [
+            {
+                emoji: "🏠",
+                text: "Проживання"
+            }
+        ],
+        starRating: 7,
+        date: "2.05.2023",
+        location: "Ulm, Baden-Württemberg"
+    },
 ];
 
 const moviesDe = [
-  {
-    id: 1,
-    title: "Infos zur medizinischen Versorgung vom Bundesgesundheitsministerium",
-    rating: [
-      {
-        emoji: "💊",
-        text: "Medizin"
-      },
-    ],
-    isPinned: true,
-    starRating: 13,
-    date: "23.07.2023"
-  },
-  {
-    id: 2,
-    title: "Kostenlose SIM-Karten der Telekom und Vodafone",
-    isHot: true,
-    rating: [  
-      {
-        emoji: "🤝",
-        text: "Networking"
-      },
-      {
-        emoji: "💸",
-        text: "Umsonst"
-      },
-    ],
-    starRating: 53,
-    date: "13.10.2023"
-  },
-  {
-    id: 3,
-    title: "Sprachkurse vor Ort",
-    rating: [  
-      {
-        emoji: "🇩🇪",
-        text: "Sprachkurs"
-      },
-      {
-        emoji: "💸",
-        text: "Umsonst"
-      },
-    ],
-    starRating: 31,
-    date: "13.09.2023"
-  },
-  {
-    id: 4,
-    title: "Jobs für ukrainische Geflüchtete",
-    rating: [  
-      {
-        emoji: "👩‍💼",
-        text: "Arbeit"
-      },
-    ],
-    starRating: 16,
-    date: "2.08.2023"
-  },
-  {
-    id: 5,
-    title: "Kostenlose Essensausgabe",
-    rating: [  
-      {
-        emoji: "🍕",
-        text: "Lebensmittel"
+    {
+        id: 1,
+        title: "Infos zur medizinischen Versorgung vom Bundesgesundheitsministerium",
+        rating: [
+            {
+                emoji: "💊",
+                text: "Medizin"
+            },
+            {
+                emoji: "💸",
+                text: "Umsonst"
+            },
+        ],
+        isPinned: true,
+        starRating: 13,
+        date: "23.07.2023",
+        location: "Ulm, Baden-Württemberg"
     },
     {
-      emoji: "💸",
-      text: "Umsonst"
+        id: 2,
+        title: "Kostenlose SIM-Karten der Telekom und Vodafone",
+        isHot: true,
+        rating: [
+            {
+                emoji: "🤝",
+                text: "Networking"
+            },
+            {
+                emoji: "💸",
+                text: "Umsonst"
+            },
+        ],
+        starRating: 53,
+        date: "13.10.2023",
+        location: "Ulm, Baden-Württemberg"
     },
-    ],
-    starRating: 15,
-    date: "2.08.2023"
-  },
-  {
-    id: 6,
-    title: "Zentrales Ankunftszentrum in der Messehalle 6",
-    rating: [  
-      {
-        emoji: "🏠",
-        text: "Wohnen"
-      }
-    ],
-    starRating: 7,
-    date: "2.05.2023"
-  },
+    {
+        id: 3,
+        title: "Sprachkurse vor Ort",
+        rating: [
+            {
+                emoji: "🇩🇪",
+                text: "Sprachkurs"
+            },
+            {
+                emoji: "💸",
+                text: "Umsonst"
+            },
+        ],
+        starRating: 31,
+        date: "13.09.2023",
+        location: "Ulm, Baden-Württemberg"
+    },
+    {
+        id: 4,
+        title: "Jobs für ukrainische Geflüchtete",
+        rating: [
+            {
+                emoji: "👩‍💼",
+                text: "Arbeit"
+            },
+        ],
+        starRating: 16,
+        date: "2.08.2023",
+        location: "Ulm, Baden-Württemberg"
+    },
+    {
+        id: 5,
+        title: "Kostenlose Essensausgabe",
+        rating: [
+            {
+                emoji: "🍕",
+                text: "Lebensmittel"
+            },
+            {
+                emoji: "💸",
+                text: "Umsonst"
+            },
+        ],
+        starRating: 15,
+        date: "2.08.2023",
+        location: "Ulm, Baden-Württemberg"
+    },
+    {
+        id: 6,
+        title: "Zentrales Ankunftszentrum in der Messehalle 6",
+        rating: [
+            {
+                emoji: "🏠",
+                text: "Wohnen"
+            }
+        ],
+        starRating: 7,
+        date: "2.05.2023",
+        location: "Ulm, Baden-Württemberg"
+    },
 ]
 
 const moviesEn = [
-  {
-    id: 1,
-    title: "Medicial care from the the department of health",
-    rating: [
-      {
-        emoji: "💊",
-        text: "Medicine"
-      },
-    ],
-    isPinned: true,
-    starRating: 13,
-    date: "23.07.2023"
-  },
-  {
-    id: 2,
-    title: "Free sim-cards by Telekom and Vodafone",
-    isHot: true,
-    rating: [
-      {
-        emoji: "🤝",
-        text: "Networking"
-      },
-      {
-        emoji: "💸",
-        text: "Free"
-      },
-    ],
-    starRating: 53,
-    date: "13.10.2023"
-  },
-  {
-    id: 3,
-    title: "Local language courses",
-    rating: [
-      {
-        emoji: "🇩🇪",
-        text: "Language Course"
-      },
-      {
-        text: "Free",
-        emoji: "💸"
-      },
-    ],
-    starRating: 31,
-    date: "13.09.2023"
-  },
-  {
-    id: 4,
-    title: "Jobs for Ukrainian refugees",
-    rating: [
-      {
-        emoji: "👩‍💼",
-        text: "Work"
-      },
-    ],
-    starRating: 16,
-    date: "2.08.2023"
-  },
-  {
-    id: 5,
-    title: "Free food distribution",
-    rating: [
-      {
-        emoji: "🍕",
-        text: "Groceries"
-      },
-      {
-        emoji: "💸",
-        text: "Free"
-      },
-    ],
-    starRating: 15,
-    date: "2.08.2023"
-  },
-  {
-    id: 6,
-    title: "Central arrival center in exhibition hall 6",
-    rating: [
-      {
-        emoji: "🏠",
-        text: "Habitation"
-      }
-    ],
-    starRating: 7,
-    date: "2.05.2023"
-  },
+    {
+        id: 1,
+        title: "Medicial care from the the department of health",
+        rating: [
+            {
+                emoji: "💊",
+                text: "Medicine"
+            },
+        ],
+        isPinned: true,
+        starRating: 13,
+        date: "23.07.2023",
+        location: "Ulm, Baden-Württemberg"
+    },
+    {
+        id: 2,
+        title: "Free sim-cards by Telekom and Vodafone",
+        isHot: true,
+        rating: [
+            {
+                emoji: "🤝",
+                text: "Networking"
+            },
+            {
+                emoji: "💸",
+                text: "Free"
+            },
+        ],
+        starRating: 53,
+        date: "13.10.2023",
+        location: "Ulm, Baden-Württemberg"
+    },
+    {
+        id: 3,
+        title: "Local language courses",
+        rating: [
+            {
+                emoji: "🇩🇪",
+                text: "Language Course"
+            },
+            {
+                text: "Free",
+                emoji: "💸"
+            },
+        ],
+        starRating: 31,
+        date: "13.09.2023",
+        location: "Ulm, Baden-Württemberg"
+    },
+    {
+        id: 4,
+        title: "Jobs for Ukrainian refugees",
+        rating: [
+            {
+                emoji: "👩‍💼",
+                text: "Work"
+            },
+        ],
+        starRating: 16,
+        date: "2.08.2023",
+        location: "Ulm, Baden-Württemberg"
+    },
+    {
+        id: 5,
+        title: "Free food distribution",
+        rating: [
+            {
+                emoji: "🍕",
+                text: "Groceries"
+            },
+            {
+                emoji: "💸",
+                text: "Free"
+            },
+        ],
+        starRating: 15,
+        date: "2.08.2023",
+        location: "Ulm, Baden-Württemberg"
+    },
+    {
+        id: 6,
+        title: "Central arrival center in exhibition hall 6",
+        rating: [
+            {
+                emoji: "🏠",
+                text: "Habitation"
+            }
+        ],
+        starRating: 7,
+        date: "2.05.2023",
+        location: "Ulm, Baden-Württemberg"
+    },
 ]
 
 const getMovies = (lang) => {
-  switch (lang) {
-    case "de": return moviesDe;
-    case "en": return moviesEn;
-    case "ua": return moviesUa
-    default: return moviesDe
-  }
+    switch (lang) {
+        case "de":
+            return moviesDe;
+        case "en":
+            return moviesEn;
+        case "ua":
+            return moviesUa
+        default:
+            return moviesDe
+    }
 }
 export const Articles = () => {
     const {
         state, setState
     } = useStateValue()
-      const [filteredArticles, setFilteredArticles] = useState(getMovies(state.language))
-      useEffect(() => {
+    const [filteredArticles, setFilteredArticles] = useState(getMovies(state.language))
+    useEffect(() => {
         setFilteredArticles((getMovies(state.language))
-          .filter(article => state.searchString ?
-            article.title.toLocaleLowerCase().includes(state.searchString)
-            || article.rating.find(r => r.text.toLocaleLowerCase().includes(state.searchString))
-          : true))
-      }, [state.searchString, state.language]) 
+            .filter(article => state.searchString ?
+                article.title.toLocaleLowerCase().includes(state.searchString)
+                || article.rating.find(r => r.text.toLocaleLowerCase().includes(state.searchString))
+                : true))
+    }, [state.searchString, state.language])
     return (
         <List>
             {filteredArticles
-            .map((movie) => (
-            <ListItem key={movie.id} movie={movie} />
-            ))}
+                .map((movie) => (
+                    <ListItem key={movie.id} movie={movie}/>
+                ))}
         </List>
     )
 }
